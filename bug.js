21d9d5033739787dccf7ffeee5f9def069fbdc8b
BugHunter-X <askzenai@gmail.com>
2025-02-05 07:50:06 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,8 @@
+```javascript
+//Some Tailwind CSS classes are not applied, even though they seem to be correctly implemented.
+
+//Example
+<div class="bg-red-500 p-4">
+  <p>This div should have a red background and padding, but it doesn't.</p>
+</div>
+```
